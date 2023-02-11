f6e60d1df8c4480d82e56436e95dfa374504a3ca
MarcinZbor <marcinzb@gazeta.pl>
2023-02-11 15:03:10 +0100

    2commit

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const prepareDOMElements = () => {
 //nadajemy nasłuchiwanie
 const prepareDOMEvents = () => {
   addBtn.addEventListener("click", addNewTodo);
+  ulList.addEventListener("click", checkClick);
 };
 
 const addNewTodo = () => {
@@ -57,6 +58,17 @@ const createToolsArea = () => {
   deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
 
   toolsPanel.append(completeBtn, editBtn, deleteBtn);
+};
+
+const checkClick = (e) => {
+  if (e.target.matches(".complete")) {
+    e.target.closest("li").classList.toggle("completed");
+    e.target.classList.toggle("completed");
+  } else if (e.target.matches(".edit")) {
+    console.log("edit");
+  } else if (e.target.matches(".delete")) {
+    console.log("delete");
+  }
 };
 
 document.addEventListener("DOMContentLoaded", main);
